2408040dd308c91f7202df0496b810b56b2cbfb2
god-s-only <joshxchris098@gmail.com>
2026-05-03 21:36:18 +0100

    fix: add @Stable to draweruistate  ui state data class

--- app/src/main/java/com/untethered/app/presentation/ui/drawer/DrawerUiState.kt
+++ app/src/main/java/com/untethered/app/presentation/ui/drawer/DrawerUiState.kt
@@ -1,8 +1,10 @@
 package com.untethered.app.presentation.ui.drawer
 
+import androidx.compose.runtime.Stable
 import com.untethered.app.domain.model.CommandHistory
 import com.untethered.app.domain.model.CommandSnippet
 
+@Stable
 data class DrawerUiState(
     val history: List<CommandHistory> = emptyList(),
     val snippets: List<CommandSnippet> = emptyList()
