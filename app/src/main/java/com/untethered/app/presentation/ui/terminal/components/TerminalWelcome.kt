1ff4b491ae29a978ce971976f296cd89a395b2f1
god-s-only <joshxchris098@gmail.com>
2026-05-03 21:32:32 +0100

    fix: terminal screen rewrite

--- app/src/main/java/com/untethered/app/presentation/ui/terminal/components/TerminalWelcome.kt
+++ app/src/main/java/com/untethered/app/presentation/ui/terminal/components/TerminalWelcome.kt
@@ -1,4 +1,4 @@
-package com.untethered.app.presentation.terminal.components
+package com.untethered.app.presentation.ui.terminal.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
